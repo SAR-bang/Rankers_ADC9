21bc8a118abf7775ee30490aee5003498af5e3a5
SAR-bang <58980785+SAR-bang@users.noreply.github.com>
2020-01-11 11:31:21 +0545

    Update models.py

--- EmployNepal/courseworkapp/models.py
+++ EmployNepal/courseworkapp/models.py
@@ -8,3 +8,12 @@ class Job(models.Model):
     def __str__(self):
         return self.job_Title + " " + self.job_discription+" "+ self.job_Catagory
 
+class Resume(models.Model):
+    name = models.CharField(max_length = 50)
+    user = models.CharField(max_length = 50)
+    resume = models.FileField(upload_to= 'resume/pdfs')
+    resume_QRcode = models.ImageField(upload_to = 'resume/images', null=True, blank = True)
+
+
+    def __str__(self):
+        return f"  the resume name is {self.name} "
