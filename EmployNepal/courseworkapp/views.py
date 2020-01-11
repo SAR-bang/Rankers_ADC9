6a6ced2f1e493d55e43d908fa2dd4dc5df29b347
SAR-bang <58980785+SAR-bang@users.noreply.github.com>
2020-01-11 11:30:14 +0545

    Update views.py

--- EmployNepal/courseworkapp/views.py
+++ EmployNepal/courseworkapp/views.py
@@ -21,3 +21,45 @@ def save(request):
         return HttpResponse("Error record saving")
 
 
+
+def upload_Resume(request):
+    return render(request, 'resume.html')
+
+def save_Resume(request):
+    context = {}
+    if request.method == 'POST':
+        get_resume = request.FILES['Resume']
+        get_image = request.FILES['imageQR']
+        get_name = request.POST['filename']
+        get_user = request.POST['username']
+        fs = FileSystemStorage()
+
+        name_resume = fs.save(get_resume.name, get_resume)
+        name_image = fs.save(get_image.name, get_image)
+        
+        print(fs.url(name_resume))
+
+        form = Resume( name = get_name, user = get_user  , resume = get_resume, resume_QRcode = get_image)
+        form.save()
+
+    else:
+        form = Resume()
+    return render(request, 'resume.html', {'form':form})
+
+
+def resumes_list(request):
+    pdfs = Resume.objects.all()
+    context = {
+        'pdfs' : pdfs
+    }
+    return render(request, 'resumelist.html',context)
+
+
+def searchResume(request):
+    resumetitle = request.GET['Search']
+
+    obj = Resume.objects.filter(name = resumetitle)
+
+    print(obj)
+
+    return render(request, 'resumelist.html', {'pdfs':obj})
